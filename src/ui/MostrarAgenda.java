e5c85e2f5cd6abd1f1f9c455c2abda7cb80dddf3
yudithleon <yudith.leon@redcap.com.co>
2025-12-18 14:17:55 -0500

    agregando un boton en la interfaz

--- src/ui/MostrarAgenda.java
+++ src/ui/MostrarAgenda.java
@@ -1,11 +1,29 @@
 package ui;
 
+import agenda.Agenda;
 import javax.swing.*;
 
 public class MostrarAgenda extends JFrame  {
+    private Agenda agenda;
+    private JPanel panel;
+    private JButton btnAgregar;
+    private JButton btnListar;
+
+
     public MostrarAgenda() {
+        agenda = new Agenda(15);
+        panel = new JPanel();
+        btnAgregar = new JButton("Añadir contacto");
+        btnListar = new JButton("Listar contactos");
+
+        panel.add(btnAgregar);
+        panel.add(btnListar);
+
+        add(panel);
+
+
         setTitle("Agenda de Contactos");
-        setSize(400, 300);
+        setSize(700, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
